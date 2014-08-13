3fc088d894d281ea018e983e75a2974a349d2931
Gabriel Izaias <gabriel.izaias@gmail.com>
2014-08-13 15:29:47 -0300

    Remove old plugin and start the new one

---
+++ src/urlToLink.js
@@ -0,0 +1,13 @@
+;(function(global, undefined){
+  'use strict';
+
+  var urlToLink = function () {
+
+  }
+
+  urlToLink.parse = function(str) {
+    return str;
+  }
+
+  return global.urlToLink = urlToLink;
+})(window)
