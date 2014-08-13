3eb42b842c9437c5c926ddca6dbf822f67616f84
Gabriel Izaias <gabriel.izaias@gmail.com>
2014-08-13 11:21:39 -0300

    Migrate gulpfile to 2 spaces

--- gulpfile.js
+++ gulpfile.js
@@ -1,42 +1,42 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify'),
     rename = require('gulp-rename'),
-    bump = require('gulp-bump');
+    bump = require('gulp-bump')
 
 gulp.task('compress', function () {
-    "use strict";
-    gulp.src('./jquery.urlToLink.js')
-        .pipe(uglify())
-        .pipe(rename('jquery.urlToLink.min.js'))
-        .pipe(gulp.dest('./'));
+  "use strict";
+  gulp.src('./jquery.urlToLink.js')
+    .pipe(uglify())
+    .pipe(rename('jquery.urlToLink.min.js'))
+    .pipe(gulp.dest('./'))
 });
 
 gulp.task('bump', function (params) {
-    "use strict";
-    gulp.src(['./package.json', './urlToLink.jquery.json'])
-        .pipe(bump())
-        .pipe(gulp.dest('./'));
+  "use strict";
+  gulp.src(['./package.json', './urlToLink.jquery.json'])
+    .pipe(bump())
+    .pipe(gulp.dest('./'))
 })
 
 gulp.task('bump:minor', function () {
-    "use strict";
-    gulp.src(['./package.json', './urlToLink.jquery.json'])
-        .pipe(bump({
-            type : 'minor'
-        }))
-        .pipe(gulp.dest('./'));
+  "use strict";
+  gulp.src(['./package.json', './urlToLink.jquery.json'])
+    .pipe(bump({
+      type : 'minor'
+    }))
+    .pipe(gulp.dest('./'))
 })
 
 gulp.task('bump:major', function () {
-    "use strict";
-    gulp.src(['./package.json', './urlToLink.jquery.json'])
-        .pipe(bump({
-            type : 'major'
-        }))
-        .pipe(gulp.dest('./'));
+  "use strict";
+  gulp.src(['./package.json', './urlToLink.jquery.json'])
+    .pipe(bump({
+      type : 'major'
+    }))
+    .pipe(gulp.dest('./'))
 })
 
 gulp.task('default', function () {
-    "use strict";
-    gulp.run('compress');
+  "use strict";
+  gulp.run('compress')
 })
